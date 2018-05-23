bf191583753e6588bdf6d7d8a35ad599e664b07c
Gopinath Rethinam <gopinathasku@gmail.com>
2018-05-23 14:06:50 +0000

    First commit

---
+++ sql-scripts/InsertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name, department, email) VALUES ('Gopinath', 'Rethinam', 'IT', 'gopi@mail.com');
+INSERT INTO employees (first_name, last_name, department, email) VALUES ('Utkarsh', 'Rana', 'IT', 'utkarsh@mail.com');
+INSERT INTO employees (first_name, last_name, department, email) VALUES ('Swapnil', 'Mote', 'IT', 'swapnil@mail.com');
