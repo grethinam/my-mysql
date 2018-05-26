4cfb064169cc75e01d16448f6a62c33f730150f9
Gopinath Rethinam <gopinathasku@gmail.com>
2018-05-26 18:13:41 +0530

    add insert statement

--- sql-scripts/InsertData.sql
+++ sql-scripts/InsertData.sql
@@ -1,3 +1,5 @@
 INSERT INTO employees (first_name, last_name, department, email) VALUES ('Gopinath', 'Rethinam', 'IT', 'gopi@mail.com');
 INSERT INTO employees (first_name, last_name, department, email) VALUES ('Utkarsh', 'Rana', 'IT', 'utkarsh@mail.com');
 INSERT INTO employees (first_name, last_name, department, email) VALUES ('Swapnil', 'Mote', 'IT', 'swapnil@mail.com');
+INSERT INTO employees (first_name, last_name, department, email) VALUES ('Fname1', 'Sname1', 'IT', 'user1@mail.com');
+INSERT INTO employees (first_name, last_name, department, email) VALUES ('Fname2', 'Sname2', 'IT', 'user2@mail.com');
