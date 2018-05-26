be8d72a81d447a9fe3f7c5058bbbc988998c1d12
Gopinath Rethinam <gopinathasku@gmail.com>
2018-05-26 18:51:39 +0530

    add insert statement

--- sql-scripts/InsertData.sql
+++ sql-scripts/InsertData.sql
@@ -3,3 +3,4 @@ INSERT INTO employees (first_name, last_name, department, email) VALUES ('Utkars
 INSERT INTO employees (first_name, last_name, department, email) VALUES ('Swapnil', 'Mote', 'IT', 'swapnil@mail.com');
 INSERT INTO employees (first_name, last_name, department, email) VALUES ('Fname1', 'Sname1', 'IT', 'user1@mail.com');
 INSERT INTO employees (first_name, last_name, department, email) VALUES ('Fname2', 'Sname2', 'IT', 'user2@mail.com');
+INSERT INTO employees (first_name, last_name, department, email) VALUES ('Fname3', 'Sname3', 'IT', 'user3@mail.com');
